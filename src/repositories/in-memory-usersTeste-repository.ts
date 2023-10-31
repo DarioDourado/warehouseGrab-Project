60bde1b47858008f6eb1148ec6b35dfe172c26e2
Dario Dourado <dariodourado@gmail.com>
2023-10-31 15:01:22 +0000

    teste register + password hash

--- src/repositories/in-memory-usersTeste-repository.ts
+++
@@ -1,7 +0,0 @@
-export class InMemoryUsersTesteRepository {
-    public usersTeste: any[] = []
-
-    async create(data: any) {
-        this.usersTeste.push(data)
-    }
-}
--- src/use-cases/userTeste.spec.ts
+++ src/use-cases/userTeste.spec.ts
@@ -1,26 +1,46 @@
+import { InMemoryUsersTesteRepository } from "@/repositories/in-memory/in-memory-usersTeste-repository";
 import { compare } from "bcryptjs";
 import { describe, expect, it } from "vitest";
 import { UserTesteRegisterUseCase } from "./userTeste-use-case";
 
 describe('userTeste Register Use Case Teste', async () => {
-    // 1º Teste a Executar
+
+    // Teste de presistência de dados, para n massacrar a db usamos a memória
+    it('Should to Register'), async () => {
+        const userTesteRepository = new InMemoryUsersTesteRepository()
+        const sut = new UserTesteRegisterUseCase(userTesteRepository)
+        
+        const { userTeste } = await sut.execute({
+            name: 'John Smith',
+            email: 'johnsmith@gmail.com',
+            password: '123456'
+        })
+        
+        expect(userTeste.id).toEqual(expect.any(Number))
+    }
+
+
+    // Teste verificação de email
     it(' Should hash user password upon registration', async () => {
 
-        const sut = new UserTesteRegisterUseCase({
-            async findByEmail(email) {
-                return null
-            },
+        const userTesteRepository = new InMemoryUsersTesteRepository()
+        const sut = new UserTesteRegisterUseCase( userTesteRepository) // Em vez de estar a refazer codigo uso o userTesteRepository
+            
+        //     ({
+        //     async findByEmail(email) {
+        //         return null
+        //     },
 
-            async create(data) {
-                return {
-                    id: 'user-1',
-                    name: data.name,
-                    email: data.email,
-                    passwordHash: data.passwordHash,
-                    createdAt: new Date()
-                }
-            },
-        })
+        //     async create(data) {
+        //         return {
+        //             id: 1,
+        //             name: data.name,
+        //             email: data.email,
+        //             passwordHash: data.passwordHash,
+        //             createdAt: new Date()
+        //         }
+        //     },
+        // })
 
         const { userTeste } = await sut.execute({
             name: 'John Smith',
